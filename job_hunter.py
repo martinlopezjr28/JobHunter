5aef8ead4e50082e25a9bbe6a3e978f0536d9512
Martin Lopez Jr <martinlopezjr28@outlook.com>
2020-03-16 14:46:24 -0700

    Add Monster class
    Added Monster class with results method stub.

---
+++ job_hunter.py
@@ -0,0 +1,12 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+
+class Monster(Object):
+
+    def __init__(self, job_name, location):
+        self.job_name = job_name
+        self.location = location
+
+    def results():
+        pass

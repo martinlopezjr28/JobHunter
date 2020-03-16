9cec10dc2f008235deb15d275e6e3e63a6cdcf9f
Martin Lopez Jr <martinlopezjr28@outlook.com>
2020-03-16 15:00:46 -0700

    Add logging and test case
    Added logging functionality with file and stream handlers.

--- job_hunter.py
+++ job_hunter.py
@@ -2,11 +2,32 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
-class Monster(Object):
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+f_string = '%(asctime)s : %(name)s : %(levelname)s - %(message)s - %(lineno)d'
+log_format = logging.Formatter(f_string)
+
+file_handler = logging.FileHandler('log/job_hunter.log')
+file_handler.setFormatter(log_format)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(log_format)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+class Monster():
 
     def __init__(self, job_name, location):
-        self.job_name = job_name
+        self.job_name = '-'.join(job_name.split(' '))
         self.location = location
 
+        logger.debug(f'Job Name: {self.job_name}')
+
     def results():
+        requests.get('https://www.monster.com/jobs/\
+                      search/?q=Software-Developer&where=Australia')
         pass
+
+monster_search = Monster('Python Developer', 'Santa Clara')
